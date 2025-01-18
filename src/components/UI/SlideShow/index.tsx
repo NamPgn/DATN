f3ad825812b5e8e8864a8608bbaa203b426cb1d9
thanhlvph46400 <thanhlvph46400@fpt.edu.vn>
2025-01-18 22:50:05 +0700

    slideshow

--- src/components/UI/SlideShow/index.tsx
+++ src/components/UI/SlideShow/index.tsx
@@ -1,42 +1,47 @@
 import Slider from "react-slick";
-
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+};
 const SlideShow = () => {
-  const settings = {
-    dots: false,
-    
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
   return (
-      <Slider {...settings} className="mt-5">
-        <div>
-          <img 
+    <Slider {...settings} className="mt-5">
+      <div>
+        <img
           className="w-100"
-            src="https://images2.thanhnien.vn/528068263637045248/2024/1/25/e093e9cfc9027d6a142358d24d2ee350-65a11ac2af785880-17061562929701875684912.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            src="https://images2.thanhnien.vn/528068263637045248/2024/1/25/e093e9cfc9027d6a142358d24d2ee350-65a11ac2af785880-17061562929701875684912.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            src="https://images2.thanhnien.vn/528068263637045248/2024/1/25/e093e9cfc9027d6a142358d24d2ee350-65a11ac2af785880-17061562929701875684912.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            src="https://images2.thanhnien.vn/528068263637045248/2024/1/25/e093e9cfc9027d6a142358d24d2ee350-65a11ac2af785880-17061562929701875684912.jpg"
-            alt=""
-          />
-        </div>
-      </Slider>
+          src="https://images2.thanhnien.vn/zoom/656_410/528068263637045248/2024/12/10/1000024907-17338163754242082979995-0-0-1123-1797-crop-173381639862577774972.jpg"
+          alt=""
+        />
+      </div>
+      <div>
+        <img
+          className="w-100"
+          src="https://images2.thanhnien.vn/zoom/656_410/528068263637045248/2025/1/3/trang-15-17355468067791340735833-47-0-1247-1920-crop-17358644184854323842.jpg"
+          alt=""
+        />
+      </div>
+      <div>
+        <img
+          className="w-100"
+          src="https://images2.thanhnien.vn/zoom/656_410/528068263637045248/2025/1/8/phao-hoa-nhin-tu-khach-san-novotel-17363062250122059181239-0-0-1600-2560-crop-17363067458141183265062.jpeg"
+          alt=""
+        />
+      </div>
+      <div>
+        <img
+          className="w-100"
+          src="https://images2.thanhnien.vn/zoom/656_410/528068263637045248/2025/1/6/ma-thien-lanh-anh-14-1736154018429118894947-0-0-1600-2560-crop-17361563485551522081241.jpg"
+          alt=""
+        />
+      </div>
+    </Slider>
   );
 };
 
